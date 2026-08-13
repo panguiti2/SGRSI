9c95ebb0400bc68323a9edbe288127fbfb7b8503
ALESSIA SOFIA VALECILLOS PETIT <62512396@ceibal.edu.uy>
2026-08-13 14:43:07 -0300

    feat: agrego carga de usuarios desde mysql. Closes #47

--- baseDeDatos/DQL/consultaUsuarioPhpMyAdmin.sql
+++ baseDeDatos/DQL/consultaUsuarioPhpMyAdmin.sql
@@ -2,7 +2,7 @@
 SELECT
     u.cedula,
     u.claveHash,
-    u.activo,
+    u.estado AS activo,
     CASE WHEN a.cedula IS NOT NULL THEN 1 ELSE 0 END AS administrador,
     CASE WHEN t.cedula IS NOT NULL THEN 1 ELSE 0 END AS tecnico,
     CASE WHEN s.cedula IS NOT NULL THEN 1 ELSE 0 END AS solicitante
@@ -15,24 +15,24 @@ WHERE u.cedula = '33333333';
 
 SELECT cedula
 FROM USUARIO
-WHERE activo = TRUE;
+WHERE estado = TRUE;
 
 
 SELECT cedula
 FROM USUARIO
-WHERE activo = FALSE;
+WHERE estado = FALSE;
 
 
-SELECT u.cedula, u.activo
+SELECT u.cedula, u.estado AS activo
 FROM USUARIO AS u
 INNER JOIN ADMINISTRADOR AS a ON a.cedula = u.cedula;
 
-SELECT u.cedula, u.activo
+SELECT u.cedula, u.estado AS activo
 FROM USUARIO AS u
 INNER JOIN TECNICO AS t ON t.cedula = u.cedula;
 
 
-SELECT u.cedula, u.activo
+SELECT u.cedula, u.estado AS activo
 FROM USUARIO AS u
 INNER JOIN SOLICITANTE AS s ON s.cedula = u.cedula;
 
@@ -77,3 +77,4 @@ LEFT JOIN TECNICO AS t
 
 LEFT JOIN SOLICITANTE AS s
     ON s.cedula = u.cedula
+ORDER BY u.cedula;

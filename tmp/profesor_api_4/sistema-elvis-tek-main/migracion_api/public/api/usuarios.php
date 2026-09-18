941efbdbddbe3532f191a777645699bea8e82ea0
ALESSIA SOFIA VALECILLOS PETIT <62512396@ceibal.edu.uy>
2026-09-18 12:12:06 -0300

    Migracion API para dispositivos y prestamos

--- tmp/profesor_api_4/sistema-elvis-tek-main/migracion_api/public/api/usuarios.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . "/../../config/config.php";
-require_once RUTA_CONTROLADOR . "/UsuarioController.php";
-
-session_start();
-
-$controlador = new UsuarioController();
-$controlador->gestionar($_SERVER["REQUEST_METHOD"]);
